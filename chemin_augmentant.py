206f6bfe628b3e5691d2c32f409e8edfa12cdb7a
harsdua <harshdua2002@gmail.com>
2023-05-18 21:37:30 +0200

    Added various flow solvers. All working

---
+++ chemin_augmentant.py
@@ -0,0 +1,31 @@
+import sys
+
+from flow_solvers.Dinitz_solver import *
+
+from time import time
+
+from flow_solvers.FF_solver import FF_solver
+
+
+def parse_file(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        num_nodes = int(lines[0].split()[1])
+        s = int(lines[1].split()[1])
+        t = int(lines[2].split()[1])
+        dSolver = Dinitz_solver(num_nodes, s, t)
+        for line in lines[4:]:
+            try:
+                source, target, capacity = map(int, line.split())
+                dSolver.add_edge(source, target, capacity)
+            except ValueError:
+                print("Error parsing line: " + line)
+        return dSolver
+dsolver = parse_file("instances/inst-1500-0.3.txt")
+
+print("done parsing")
+input()
+t1 = time()
+print(dsolver.get_max_flow())
+t2 = time()
+print(t2-t1)
